49d6ec6b3874dbc9125ac7015a1b62d1414c7981
vikrant <vikrant.immeasurable@gmail.com>
2019-03-08 08:57:58 +0530

    callback and Error handling

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ var rect= require('./rectangle');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + "and b = " +b);
+    
+    rect(l,b, (err, rectangle) => {
+        if(err) {
+            console.log("ERROR:", err.message);
+        }
+        else {
+            console.log("The area of the reactangle of dimmensions l = " 
+            + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimmensions l = " 
+            + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
 
-    if( l <= 0 || b <= 0) {
-        console.log("Rectangle dimmensions should be greater than zero: l = " + l + ", and b = " +b);
-    }
-    else {
-        console.log("The area of rectangle is " + rect.area(l,b));
-        console.log("The perimeter of rectangle is " + rect.perimeter(l,b));
-    }
+    console.log("This statement is after the call to rect()")
 }
 
 solveRect(2,4);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,34 @@
-exports.perimeter =  (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (x,y,callback) => {
+    if( l <= 0 || b <= 0) {
+        setTimeout(() => 
+            callback( new Error("Rectangle dimmensions should be greater than zero: l = " 
+                + x + ", and b = " + y),
+            null) ,
+            2000)
+    }
+    else {
+        setTimeout(() => 
+            callback( null,
+            {
+                perimeter :  (x,y) => (2*(x+y)),
+                area : (x,y) => (x*y)
+            }) ,
+            2000)
+
+    }
+
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
